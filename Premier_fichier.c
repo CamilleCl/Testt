33134a48139d0fa1b0009d76dd70060ce647725e
CamilleCl <camilleclabodtsrgmail.com>
2021-03-22 14:51:23 +0100

    Ajout fichier1 C

---
+++ Premier_fichier.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, const char *argv[])
+{
+    printf("Bonjour, je suis le premier fichier hehe");
+    return 0;
+}
---
+++ Second_fichier.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, const char *argv[])
+{
+    printf("hola hola, ik ben de tweede");
+    return 0;
+}
